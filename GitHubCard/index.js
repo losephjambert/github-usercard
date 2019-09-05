8b830fdcafbef31c3f25dd49f019719b45fa9e08
Joe <josephc.lambert@gmail.com>
2019-09-05 13:52:04 -0700

    adds createElement() util fn

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -116,6 +116,14 @@ const followersArray = [];
 </div>
 
 */
+function createElement(type, classes = null, id = null) {
+  const element = document.createElement(type);
+
+  classes !== null && element.classList.add(...classes);
+  id !== null && element.setAttribute('id', id);
+
+  return element;
+}
 
 function UserCard(data) {}
 
