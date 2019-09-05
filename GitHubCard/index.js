9c099594bacf5e2e67b149bc01854a43f8874c05
Joe <josephc.lambert@gmail.com>
2019-09-05 13:51:22 -0700

    makes axios call for my info. adds stubbed data to avoid rate limiting from github. adds UserCard fn declaration

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,25 +1,96 @@
-/* Step 1: using axios, send a GET request to the following URL 
+/* Step 1: using axios, send a GET request to the following URL
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+// const githubURL = 'https://api.github.com/users/losephjambert';
+// axios
+//   .get(githubURL)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(`there was a problem requesting ${githubURL}`, err);
+//   });
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+const stubbedResponse = {
+  data: {
+    login: 'losephjambert',
+    id: 16310372,
+    node_id: 'MDQ6VXNlcjE2MzEwMzcy',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/16310372?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/losephjambert',
+    html_url: 'https://github.com/losephjambert',
+    followers_url: 'https://api.github.com/users/losephjambert/followers',
+    following_url: 'https://api.github.com/users/losephjambert/following{/other_user}',
+    gists_url: 'https://api.github.com/users/losephjambert/gists{/gist_id}',
+    starred_url: 'https://api.github.com/users/losephjambert/starred{/owner}{/repo}',
+    subscriptions_url: 'https://api.github.com/users/losephjambert/subscriptions',
+    organizations_url: 'https://api.github.com/users/losephjambert/orgs',
+    repos_url: 'https://api.github.com/users/losephjambert/repos',
+    events_url: 'https://api.github.com/users/losephjambert/events{/privacy}',
+    received_events_url: 'https://api.github.com/users/losephjambert/received_events',
+    type: 'User',
+    site_admin: false,
+    name: 'Joseph Lambert',
+    company: 'freelance',
+    blog: 'twitter.com/losephjambert',
+    location: 'Seattle, WA',
+    email: null,
+    hireable: null,
+    bio: 'www things\r\nwww people',
+    public_repos: 32,
+    public_gists: 0,
+    followers: 6,
+    following: 2,
+    created_at: '2015-12-15T18:44:43Z',
+    updated_at: '2019-07-29T15:02:03Z',
+  },
+  status: 200,
+  statusText: 'OK',
+  headers: {
+    'cache-control': 'public, max-age=60, s-maxage=60',
+    'content-type': 'application/json; charset=utf-8',
+    etag: 'W/"c67da2d7b5b6566fa882e049a43a1261"',
+    'last-modified': 'Mon, 29 Jul 2019 15:02:03 GMT',
+    'x-github-media-type': 'github.v3',
+    'x-ratelimit-limit': '60',
+    'x-ratelimit-remaining': '59',
+    'x-ratelimit-reset': '1567719492',
+  },
+  config: {
+    url: 'https://api.github.com/users/losephjambert',
+    method: 'get',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+    },
+    transformRequest: [null],
+    transformResponse: [null],
+    timeout: 0,
+    xsrfCookieName: 'XSRF-TOKEN',
+    xsrfHeaderName: 'X-XSRF-TOKEN',
+    maxContentLength: -1,
+  },
+  request: {},
+};
+
+/* Step 2: Inspect and study the data coming back, this is YOUR
+   github info! You will need to understand the structure of this
+   data in order to use it to build your component function
 
    Skip to Step 3.
 */
 
-/* Step 4: Pass the data received from Github into your function, 
+/* Step 4: Pass the data received from Github into your function,
            create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
+/* Step 5: Now that you have your own card getting added to the DOM, either
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers
+          , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
-          
+
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
@@ -35,7 +106,7 @@ const followersArray = [];
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -46,7 +117,9 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
+function UserCard(data) {}
+
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
